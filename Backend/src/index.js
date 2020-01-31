57f067c1dcb84978a5c9f72d64836a2114df9074
JoaoVictorCoder <55103977+JoaoVictorCoder@users.noreply.github.com>
2020-01-31 19:31:50 -0300

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://joaovictor:uiakatcha@cluster0-ezyyh.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://usernamedb:password@cluster0-ezyyh.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
